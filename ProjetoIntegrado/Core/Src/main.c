19d3b0140d2b693d10a5ecefa9fa3bff44f9b7b9
layonfreitas <lyon.rub@gmail.com>
2026-05-11 09:57:12 -0300

    terminando

--- ProjetoIntegrado/Core/Src/main.c
+++ ProjetoIntegrado/Core/Src/main.c
@@ -20,6 +20,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "st7735\st7735.h" // display
